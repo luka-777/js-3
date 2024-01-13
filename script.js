b427b9cbcaf48867a8a2ab1d409a2590df2bf5f9
luka-777 <lukagiunashvili2@gmail.com>
2024-01-13 03:07:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// 1)
+
+// let number = "0";{
+//     if (number > 0) {
+//         console.log("დადებითია");
+//     } else if (number < 0) {
+//         console.log("უარყოფითია");
+//     } else {
+//         console.log("არც ერთი");
+//     }
+// }
+
+
+
+
+// 2)
+
+// function calculator(num1, num2, operator) {
+//     let result;
+
+//     switch (operator) {
+//         case '+':
+//             result = num1 + num2;
+//             break;
+//         case '-':
+//             result = num1 - num2;
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             break;
+//         case '/':
+//             result = num1 / num2;
+//             break;
+//     }
+// }
+
+
+
+
+// 4)
+
+// let Name = "";
+// let lastName = "";
+// let email = "";
+// let password = "";
+// let phoneNumber = "";
+
+// if (Name !== "" && lastName !== "" && email === "" && password.length > 8) {
+//     console.log ("თქვენ დარეგისტრირებული ხართ");
+// } else {
+//     console.log ("გთხოვთ გადაამოწმოთ შეტანილი მონაცემები")
+// }
